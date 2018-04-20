61e1c1e8ce2310507b4a2b1731d5707b045c022d
JayJayEvans <jason_evans2@baylor.edu>
2018-04-19 21:13:55 -0500

    Init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+// const hbs = require('hbs');
+// const fs = require('fs');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000; 
+var app = express();
+
+app.use(express.static(publicPath)); //middleware
+
+app.listen(3000, () => {
+  console.log(`Server is up on port ${port}`);
+});
+
+ 
